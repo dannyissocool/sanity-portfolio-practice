efdfde6a0d946f616c147a55053974e9c2440183
Danny Meuse <danny.meuse@gmail.com>
2021-09-11 21:49:40 -0700

    entire portfolio project

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import image from "../mountain_small.jpg";
+
+const Home = () => {
+  return (
+    <main>
+      <img
+        src={image}
+        alt="mountain valley in nevada"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hello! I'm Danny.
+        </h1>
+      </section>
+    </main>
+  );
+};
+export default Home;
